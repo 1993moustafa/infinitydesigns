dca7d3c112556b4fa1fd580bcfb2bda87f32ff60
Moustafa Omar <1993moustafa@gmail.com>
2019-04-05 08:41:38 +0200

    Jumbotron

--- components/Jumbotron.js
+++ components/Jumbotron.js
@@ -166,7 +166,6 @@ class JumbotronMineshaft extends Component {
               <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
               <hr className="my-2" />
               <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            
             </Jumbotron>
           </Element>
 
